fe9b4d5c2432beb80fa7ade6ea0f8c08cf1ff384
dwolf1 <dannawolf1@gmail.com>
2016-11-01 16:33:13 -0400

    validations user/boat/job

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :email, uniqueness: true
   has_many :jobs
   has_many :boats
 end
