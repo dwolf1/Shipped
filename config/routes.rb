1a8d549485c1d795c3cff720a4a87e5a94948120
dwolf1 <dannawolf1@gmail.com>
2016-11-01 17:00:21 -0400

    as paths added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   	# Boat Routes
-	get    '/boats' 	 	  	=> 'boats#index'
-	get    '/boats/new' 	  	=> 'boats#new'
+	get    '/boats' 	 	  	=> 'boats#index', as: "boats"
+	get    '/boats/new' 	  	=> 'boats#new', as: "new_boat"
 	post   '/boats' 		  	=> 'boats#create'
-	get    '/boats/:id' 	  	=> 'boats#show'
-	get    '/boats/:id/edit' 	=> 'boats#edit'
+	get    '/boats/:id' 	  	=> 'boats#show', as: "boat"
+	get    '/boats/:id/edit' 	=> 'boats#edit', as: "edit_boat"
 	put    '/boats/:id'	  		=> 'boats#update'
 	patch  '/boats/:id'	  		=> 'boats#update'
 	delete '/boats/:id' 	  	=> 'boats#destroy'
 
 	# Job Routes
-	get    '/jobs' 	 	  	=> 'jobs#index'
-	get    '/jobs/new' 	  	=> 'jobs#new'
+	get    '/jobs' 	 	  	=> 'jobs#index', as: "jobs"
+	get    '/jobs/new' 	  	=> 'jobs#new', as: "new_job"
 	post   '/jobs' 		  	=> 'jobs#create'
-	get    '/jobs/:id' 	  	=> 'jobs#show'
-	get    '/jobs/:id/edit' => 'jobs#edit'
+	get    '/jobs/:id' 	  	=> 'jobs#show', as: "job"
+	get    '/jobs/:id/edit' => 'jobs#edit', as: "edit_job"
 	put    '/jobs/:id'	  	=> 'jobs#update'
 	patch  '/jobs/:id'		=> 'jobs#update'
 	delete '/jobs/:id' 	  	=> 'jobs#destroy'
