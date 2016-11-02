4a874c5663413a281be0576773b0f598e28d2e0f
6ytrenches <miguelmyers8@gmail.com>
2016-11-02 14:13:48 -0400

    jobs show and new fixed

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,6 +7,7 @@ class JobsController < ApplicationController
   end
 
   def show
+     @job = Job.find(params[:id])
   end
 
   def edit
@@ -17,15 +18,27 @@ class JobsController < ApplicationController
 
   def new
     @message = "form for jober. after devics sign up that users should be drected to this page"
-  @job = Job.new(title: params[:title], origin: params[:origin], destination: params[:destination])
+  @job = Job.new
   end
 
   def create
-    @job = Job.new()
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to job_path(@job)
+    else 
+      render new_job_path
+    end
   end
 
 
   def destroy
+  end
+
+  #--------------
+private 
+
+ def job_params
+        params.require(:job).permit(:title, :description, :origin, :destination, :cost, :cost_of_job, :user_id)
   end
 
 end
