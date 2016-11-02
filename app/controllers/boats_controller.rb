2abdcdba53238eaf42f5e9048bc27efc4fe28652
Kevin Plaza <kplaza1987@gmail.com>
2016-11-02 10:48:38 -0400

    boats

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,29 @@
 class BoatsController < ApplicationController
+
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+
+  @boat = Boat.new(name: params[:name], amount_that_can_be_shipped: params[:amount_that_can_be_shipped], location: params[:location])
+  end
+
+  def create
+    @boat = Boat.new()
+  end
+
+
+  def destroy
+  end
+
 end
