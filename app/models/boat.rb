4e4145a91250e38562cea4b5a2af91abd0796e58
dwolf1 <dannawolf1@gmail.com>
2016-11-04 16:37:52 -0400

    layout changes

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,17 +1,16 @@
 class Boat < ApplicationRecord
   	has_and_belongs_to_many :jobs
   	# belongs_to :users
-has_many :assignments
-has_many :jobs, through: :assignments
-
+	has_many :assignments
+	has_many :jobs, through: :assignments
 
   	validates :name, uniqueness: true
 
-has_attached_file :avatar,
- :styles => { :medium => "300x300>", :thumb => "200x200>" },
- :default_url => "missing.png"
+	has_attached_file :avatar,
+ 	:styles => { :medium => "300x300>", :thumb => "200x200>" },
+ 	:default_url => "missing.png"
 
-validates_attachment_content_type :avatar,
- :content_type => /\Aimage\/.*\Z/
+	validates_attachment_content_type :avatar,
+ 	:content_type => /\Aimage\/.*\Z/
 
  end
